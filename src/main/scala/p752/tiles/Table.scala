679b055690fbff65278aed5df249c89b0f8c5d8d
S.H.Ayat <fahim.ayat@gmail.com>
2022-12-25 08:38:30 +0330

    better table model

--- src/main/scala/p752/tiles/Table.scala
+++ src/main/scala/p752/tiles/Table.scala
@@ -4,18 +4,19 @@ import p752.Tiles.*
 import p752.*
 
 case class Table[T](
-    items: List[T],
+    value: T,
     headers: List[String],
-    show: T => List[String],
+    show: T => List[List[String]],
+    title: Option[String],
     align: Align.Horizontal = Align.Horizontal.Center,
     index: Int = 0,
     headerStyle: Style = Table.headerStyle,
     defaultStyle: Style = Table.itemsStyle,
     selectedStyle: Style = Table.selectedStyle
 ) extends Tile[Any] {
+  private val data = show(value)
   private val sep = "  "
   override val render: String = {
-    val data = items.map(show)
     val lens = data.foldLeft(headers.map(_.pureSize)) { (acc, curr) =>
       acc.zip(curr).map { (len, item) =>
         val l = item.pureSize
@@ -50,11 +51,11 @@ case class Table[T](
     case Left(e) =>
       e match {
         case Event.Key('j') | Event.Special.Down =>
-          val ind = (index + 1) % items.length
+          val ind = (index + 1) % data.length
           this.copy(index = ind)
 
         case Event.Key('k') | Event.Special.Up =>
-          val ind = if index == 0 then items.length - 1 else index - 1
+          val ind = if index <= 0 then data.length - 1 else index - 1
           this.copy(index = ind)
         case _ =>
           this
